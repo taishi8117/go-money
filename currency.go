e4bfd539aecced8ac24e39badd56715d4b1520e0
Taishi Nojima <taishi.nojima@aya.yale.edu>
2020-11-23 23:16:44 +0900

    add USDT

--- currency.go
+++ currency.go
@@ -187,8 +187,9 @@ var currencies = map[string]*Currency{
 	"ZMW": {Decimal: ".", Thousand: ",", Code: "ZMW", Fraction: 2, Grapheme: "ZK", Template: "$1"},
 	"ZWD": {Decimal: ".", Thousand: ",", Code: "ZWD", Fraction: 2, Grapheme: "Z$", Template: "$1"},
 
-	"BTC": {Decimal: ".", Thousand: ",", Code: "BTC", Fraction: 8, Grapheme: "\u0e3f", Template: "$1"},
-	"ETH": {Decimal: ".", Thousand: ",", Code: "ETH", Fraction: 8, Grapheme: "\u039e", Template: "$1"},
+	"BTC":  {Decimal: ".", Thousand: ",", Code: "BTC", Fraction: 8, Grapheme: "\u0e3f", Template: "$1"},
+	"ETH":  {Decimal: ".", Thousand: ",", Code: "ETH", Fraction: 8, Grapheme: "\u039e", Template: "$1"},
+	"USDT": {Decimal: ".", Thousand: ",", Code: "USDT", Fraction: 2, Grapheme: "$", Template: "$1"},
 }
 
 // AddCurrency lets you insert or update currency in currencies list.
